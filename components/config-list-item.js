3611d8f1cfe8d0809fc4a2859b0bc6b0a54f249e
Taylor Fraley <tfraley@clearent.com>
2021-12-03 21:07:11 -0500

    removed div

--- components/config-list-item.js
+++ components/config-list-item.js
@@ -2,14 +2,12 @@ import Link from 'next/link'
 
 export default function ConfigListItem({filename}) {
   return (
-    <div>
-      <div>
-        <Link href={`/${encodeURIComponent(filename)}`}>
-          <a>
-            <p>{filename.replace('.json', '')}</p>
-          </a>
-        </Link>
-      </div>
+    <div className="border-2 my-2 p-2 rounded">
+      <Link href={`/${encodeURIComponent(filename)}`}>
+        <a>
+          <p>{filename.replace('.json', '')}</p>
+        </a>
+      </Link>
     </div>
   )
 }
