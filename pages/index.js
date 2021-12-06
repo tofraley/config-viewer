d7410c8461ad502da8fe7c0a46f2680b68d62027
Taylor Fraley <tfraley@clearent.com>
2021-12-06 16:32:57 -0500

    use index of selected file instead of the file itself; play with styles

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export async function getStaticProps() {
 
 export default function Home({files}) {
   const [filteredFiles, setFilteredFiles] = useState(files);
-  const [selectedFile, setSelectedFile] = useState(files[0]);
+  const [selectedFile, setSelectedFile] = useState(0);
   return (
     <div className="container ml-8 my-3">
       <div className="grid gap-2 grid-cols-2">
@@ -45,14 +45,12 @@ export default function Home({files}) {
                 files.filter((f) => f.filename.toLowerCase().startsWith(e.target.value.toLowerCase())))
             }
             />
-          <div className="border-solid border-4 border-blue-300 rounded-lg mt-2 min-h-full">
+          <div className="border-solid border-4 border-blue-300 rounded-lg mt-1 min-h-full">
             {filteredFiles.map((file, i) => (
               <h2
-                className="pl-3 p-1 m-1 rounded text-lg cursor-pointer hover:bg-blue-200"
+                className={`pl-3 p-1 m-1 rounded text-lg cursor-pointer`}
                 key={i} 
-                onClick={(e) => setSelectedFile(
-                  file
-                )}
+                onClick={(e) => setSelectedFile(i)}
                 >
                   {file.filename.replace('.json', '')}
               </h2>
@@ -60,12 +58,14 @@ export default function Home({files}) {
           </div>
         </div>
         <div>
-        <div className="text-center">
-          <h1 className="text-xl mt-1">Config Viewer</h1>
-        </div>
-        <div className="min-w-max min-h-full max-w-full p-4 mt-4 border-solid border-4 border-green-300 rounded-lg">
-          <Inspector data={selectedFile.content} />
-        </div>
+            <div className="text-lg border-solid border-4 border-green-300 rounded-lg pl-2 h-10 w-full">
+              <h1 className="">{files[selectedFile].filename.replace('.json', '') || "Config Viewer"}</h1>
+            </div>
+          <div className="mt-1 min-w-max min-h-full max-w-full border-solid border-4 border-green-300 rounded-lg">
+            <div className="p-2">
+              <Inspector data={files[selectedFile].content} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
