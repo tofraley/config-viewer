c00f0a796c5ac671893250f931b03516275e8132
Taylor Fraley <tfraley@clearent.com>
2021-12-06 17:11:10 -0500

    move container div to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,10 @@ import '../styles/json-inspector.css'
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <div className="container ml-8 my-3">
+      <Component {...pageProps} />
+    </div>
+  )}
 
 export default MyApp
