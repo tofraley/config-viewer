a5c471d0bd1aaf0a486ff083ad54d65bb20a29c4
Taylor Fraley <tfraley@clearent.com>
2021-12-06 20:52:07 -0500

    playing with styles

--- pages/index.js
+++ pages/index.js
@@ -32,51 +32,45 @@ export async function getStaticProps() {
 export default function Home({files}) {
   const [selectedFile, setSelectedFile] = useState(0);
   return (
-      <div className="grid gap-2 grid-cols-2">
-        <ConfigSelector 
-          files={files}
-          setSelectedFile={setSelectedFile} />
-        <ConfigDisplay { ...files[selectedFile]} />
-      </div>
+    <div className="grid gap-2 grid-cols-2">
+      <ConfigSelector 
+        files={files}
+        setSelectedFile={setSelectedFile} />
+      <ConfigDisplay { ...files[selectedFile]} />
+    </div>
   )
 }
 
 function ConfigSelector({files, setSelectedFile}) {
   const [filteredFiles, setFilteredFiles] = useState(files);
   return (
-        <div className="">
-          <input 
-            placeholder="Search..."
-            className="text-lg border-solid border-4 border-blue-300 rounded-lg p-3 h-10 w-full" type="search"  
-            onChange={(e) => setFilteredFiles(
-                files.filter((f) => f.filename.toLowerCase().startsWith(e.target.value.toLowerCase()))
-          )}
-            />
-          <div className="border-solid border-4 border-blue-300 rounded-lg mt-1 min-h-full">
-            {filteredFiles.map((file, i) => (
-              <h2
-                className={`pl-3 p-1 m-1 rounded text-lg cursor-pointer`}
-                key={i} 
-                onClick={(e) => setSelectedFile(i)}
-                >
-                  {file.filename.replace('.json', '')}
-              </h2>
-            ))}
-          </div>
-        </div>
+    <div className="border-solid border-4 border-blue-300 rounded-lg">
+      <input 
+        placeholder="Search..."
+        className="m-2 text-lg p-1 h-10 rounded border-2 border-blue-300 focus:outline-none" type="search"  
+        onChange={(e) => setFilteredFiles(
+            files.filter((f) => f.filename.toLowerCase().startsWith(e.target.value.toLowerCase()))
+      )}
+        />
+      <div className="pl-2 mt-1 min-h-full">
+        {filteredFiles.map((file, i) => (
+          <h2
+            className="m-1 rounded text-lg cursor-pointer hover:bg-blue-200"
+            key={i} 
+            onClick={(e) => setSelectedFile(i)}
+            >
+              {file.filename.replace('.json', '')}
+          </h2>
+        ))}
+      </div>
+    </div>
   )
 }
+
 function ConfigDisplay(file) {
   return (
-        <div>
-            <div className="text-lg border-solid border-4 border-green-300 rounded-lg pl-2 h-10 w-full">
-              <p className="">{file.filename.replace('.json', '') || "Config Viewer"}</p>
-            </div>
-          <div className="mt-1 min-w-max min-h-full max-w-full border-solid border-4 border-green-300 rounded-lg">
-            <div className="p-2">
-              <Inspector data={file.content} />
-            </div>
-          </div>
-        </div>
+    <div className="border-4 border-solid border-green-300 rounded">
+      <Inspector className="text-lg font-sans" data={file.content} />
+    </div>
   )
 }
