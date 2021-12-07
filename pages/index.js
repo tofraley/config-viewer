38a301c6616ebfdaca2dcb7c8ed16902315de03f
Taylor Fraley <tfraley@clearent.com>
2021-12-07 10:43:41 -0500

    extract LeftBox and RightBox

--- pages/index.js
+++ pages/index.js
@@ -25,52 +25,87 @@ export async function getStaticProps() {
       content: content
     }
   });
-  return { props: { files }}
+
+  return { props: { files }};
 }
 
 export default function Home({files}) {
+
   const [selectedFile, setSelectedFile] = useState(0);
+
   return (
     <div className="grid gap-2 grid-cols-2">
-      <ConfigSelector 
-        files={files}
-        selectedFile={selectedFile}
-        setSelectedFile={setSelectedFile} />
-      <ConfigDisplay { ...files[selectedFile]} />
+      <LeftBox>
+        <ConfigSelector files={files} selectedFile={selectedFile} setSelectedFile={setSelectedFile} />
+      </LeftBox>
+      <RightBox>
+        <Inspector className="text-lg font-sans" data={files[selectedFile].content} />
+      </RightBox>
     </div>
-  )
+  );
+}
+
+const SelectorSearch = ({files, changeHandler}) => {
+  return (
+    <input 
+      placeholder="Search..."
+      className="mt-2 ml-2 text-lg pl-1 h-10 rounded border-2 border-blue-300 focus:outline-none w-1/2" type="search"  
+      onChange={changeHandler} />
+  );
+}
+
+const FileItem = ({file, isSelected, clickHandler}) => {
+  return (
+    <h2
+      className={`${isSelected ? 'bg-blue-100' : ''} pl-1 rounded text-lg cursor-pointer hover:bg-blue-200`}
+      onClick={clickHandler}
+      >
+        {file.filename.replace('.json', '')}
+    </h2>
+  );
+}
+
+const LeftBox = ({children}) => {
+  return (
+    <div className="border-solid border-4 border-blue-300 rounded-lg">
+      { children }
+    </div>
+  );
+}
+
+const FileList = ({files, selectedFile, setSelectedFile}) => {
+  return (
+      <div className="ml-2 mr-2 min-h-full">
+        {files.map((file, i) => (
+          <FileItem key={i} file={file} 
+            isSelected={(selectedFile == i)}
+            clickHandler={(e) => {
+              setSelectedFile(i)
+            }}/>
+        ))}
+      </div>
+  );
 }
 
 function ConfigSelector({files, selectedFile, setSelectedFile}) {
   const [filteredFiles, setFilteredFiles] = useState(files);
+  const filter = (terms) => 
+            setFilteredFiles(
+              files.filter((f) => f.filename.toLowerCase().startsWith(terms.toLowerCase()))
+            );
   return (
-    <div className="border-solid border-4 border-blue-300 rounded-lg">
-      <input 
-        placeholder="Search..."
-        className="mt-2 ml-2 text-lg pl-1 h-10 rounded border-2 border-blue-300 focus:outline-none w-1/2" type="search"  
-        onChange={(e) => setFilteredFiles(
-            files.filter((f) => f.filename.toLowerCase().startsWith(e.target.value.toLowerCase()))
-      )}
-        />
-      <div className="ml-2 mr-2 min-h-full">
-        {filteredFiles.map((file, i) => (
-          <h2
-            className={`${selectedFile == i ? 'bg-blue-100' : ''} pl-1 rounded text-lg cursor-pointer hover:bg-blue-200`}
-            key={i} 
-            onClick={(e) => setSelectedFile(i)}
-            >
-              {file.filename.replace('.json', '')}
-          </h2>
-        ))}
-      </div>
-    </div>
-  )
+    <>
+      <SelectorSearch files={files} changeHandler={(e) => filter(e.target.value)} />
+      <FileList files={filteredFiles} setSelectedFile={setSelectedFile} selectedFile={selectedFile} />
+    </>
+  );
+
 }
 
-function ConfigDisplay(file) {
+function RightBox({children}) {
   return (
     <div className="border-4 border-solid border-green-300 rounded">
-      <Inspector className="text-lg font-sans" data={file.content} />
+      { children }
     </div>
-  )
+  );
 }
