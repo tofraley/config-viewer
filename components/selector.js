83cc591618807fb910b696b3e1932f363e67472e
Taylor Fraley <tfraley@clearent.com>
2021-12-19 15:46:19 -0500

    added group feature

--- components/selector.js
+++ components/selector.js
@@ -2,25 +2,19 @@ import List from "./list";
 import Search from "./search";
 import { useState } from "react";
 
-export default function Selector({groups, selectedItem, setSelectedItem}) {
-  const [filteredGroups, setFilteredGroups] = useState(groups);
+export default function Selector({data, selectedItem, setSelectedItem}) {
+  const [filteredData, setFilteredData] = useState(data);
   const filter = 
     (terms) => (
-      setFilteredGroups(
-        groups.map((data, i) => data.filter((d) => d.toLowerCase().startsWith(terms.toLowerCase())))
+      setFilteredData(
+        data.filter((d) => d.toLowerCase().startsWith(terms.toLowerCase()))
       )
     );
-  console.log(groups)
 
   return (
     <>
       <Search changeHandler={(e) => filter(e.target.value)} />
-        {filteredGroups.map((group) => (
-          <div key={group.name}>
-            <h3>{group.name}</h3>
-            <List data={group.data} setSelectedItem={setSelectedItem} selectedItem={selectedItem} />
-          </div>
-        ))}
+      <List data={filteredData} setSelectedItem={setSelectedItem} selectedItem={selectedItem} />
     </>
   );
 }
