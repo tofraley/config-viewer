93e19517b62da9ed70e35ba1fd3adcb96d7a2f2f
Taylor Fraley <tfraley@clearent.com>
2021-12-20 10:41:36 -0500

    move search out of feature components

--- src/components/selector.js
+++ src/components/selector.js
@@ -1,18 +1,21 @@
 import List from "./list";
 import Search from "./search";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Feature from '../components/feature'
 import UnlessFeature from "./unless-feature";
 import useGroups from '../services/groupService'
 import ListItem from './list-item'
 import Group from './group'
+import FeatureContext from '../components/feature-context'
 
 export default function Selector({data, selectedItem, setSelectedItem}) {
   const [groups, setGroups] = useGroups(data)
   const [filteredData, setFilteredData] = useState(data)
   const [filteredGroups, setFilteredGroups] = useState(groups)
+  const features = useContext(FeatureContext)
+  const isGroupFeatureOn = features.find(f => f === 'group')
 
-  const filter = 
+  const filterData = 
     (terms) => (
       setFilteredData(
         data.filter((d) => d.toLowerCase().startsWith(terms.toLowerCase()))
@@ -31,28 +34,32 @@ export default function Selector({data, selectedItem, setSelectedItem}) {
       )
     );
 
+  function filter(terms) {
+    if(isGroupFeatureOn) { 
+      return filterGroups(terms)
+    }
+    return filterData(terms)
+  }
+
   return (
     <>
-      <Feature name='group'>
-        <Search changeHandler={(e) => filterGroups(e.target.value)} />
-        <List>
-          {filteredGroups.map(({id, name, data, isOpen}) => (
-            <Group key={name} {...{id, name, isOpen, data, selectedItem, setSelectedItem}} />
-          ))}
-        </List>
-      </Feature>
-      <UnlessFeature name='group'>
         <Search changeHandler={(e) => filter(e.target.value)} />
         <List>
-          {filteredData.map((file) => (
-            <ListItem key={file} display={file} 
-              isSelected={(selectedItem == file)}
-              clickHandler={(_) => (
-                setSelectedItem(file)
-              )}/>
-          ))}
+          <Feature name='group'>
+            {filteredGroups.map(({id, name, data, isOpen}) => (
+              <Group key={name} {...{id, name, isOpen, data, selectedItem, setSelectedItem}} />
+            ))}
+          </Feature>
+          <UnlessFeature name='group'>
+              {filteredData.map((file) => (
+                <ListItem key={file} display={file} 
+                  isSelected={(selectedItem == file)}
+                  clickHandler={(_) => (
+                    setSelectedItem(file)
+                  )}/>
+              ))}
+          </UnlessFeature>
         </List>
-      </UnlessFeature>
     </>
   );
 }
